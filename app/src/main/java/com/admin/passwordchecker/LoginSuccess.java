5ab980689c61049475935ab0792cd51d3c5a849e
jzyirek <bluecat1>
2017-09-25 16:20:57 -0700

    v0.1

---
+++ app/src/main/java/com/admin/passwordchecker/LoginSuccess.java
@@ -0,0 +1,14 @@
+package com.admin.passwordchecker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginSuccess extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_success);
+    }
+}
