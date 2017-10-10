eae373379c042c4478455f4b9883b86e0614daec
Joe Zyirek <jzyirek@gmail.com>
2017-10-09 21:43:53 -0700

    v3

--- app/src/main/java/com/admin/passwordchecker/PasswordCheckerActivity.java
+++ app/src/main/java/com/admin/passwordchecker/PasswordCheckerActivity.java
@@ -6,16 +6,17 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class PasswordCheckerActivity extends AppCompatActivity implements OnClickListener
+public class PasswordCheckerActivity extends AppCompatActivity implements OnClickListener, TextView.OnEditorActionListener
 {
     private EditText UsernameInputId;
     private EditText PasswordInputId;
-    private TextView PasswordCorrectId;
     private Button  SubmitButtonId;
+    private String username = "jzyirek";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,17 +26,16 @@ public class PasswordCheckerActivity extends AppCompatActivity implements OnClic
 
         UsernameInputId = (EditText) findViewById(R.id.UsernameInputId);
         PasswordInputId = (EditText) findViewById(R.id.PasswordInputId);
-        PasswordCorrectId = (TextView) findViewById(R.id.PasswordCorrectId);
         SubmitButtonId = (Button) findViewById(R.id.SubmitButtonId);
 
+        PasswordInputId.setOnEditorActionListener(this);
         SubmitButtonId.setOnClickListener(this);
+        SubmitButtonId.setClickable(false);
     }
 
     public void verifyPassword()
     {
-        String username = "jzyirek";
         String validPassword = "calculate";
-        String incorrectPassword = "Wrong!";
         String userIdTest = UsernameInputId.getText().toString();
         String passwordIdTest = PasswordInputId.getText().toString();
 
@@ -43,16 +43,12 @@ public class PasswordCheckerActivity extends AppCompatActivity implements OnClic
         {
             if(passwordIdTest.equals(validPassword))
             {
-                loginSuccess(username);
-            }
-            else
-            {
-                PasswordCorrectId.setText(incorrectPassword);
+                SubmitButtonId.setClickable(true);
             }
         }
         else
         {
-            PasswordCorrectId.setText("Invalid Username");
+            PasswordInputId.getHint();
         }
     }
 
@@ -68,7 +64,18 @@ public class PasswordCheckerActivity extends AppCompatActivity implements OnClic
     {
         if (view.getId() == R.id.SubmitButtonId)
         {
+            loginSuccess(username);
+        }
+    }
+
+    @Override
+    public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent)
+    {
+        if (actionId == EditorInfo.IME_ACTION_DONE ||
+                actionId == EditorInfo.IME_ACTION_UNSPECIFIED)
+        {
             verifyPassword();
         }
+        return false;
     }
 }
